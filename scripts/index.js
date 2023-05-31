be3330688f77364f09ad5e8de416a50865b7e6c4
prodSlatt <124645710+prodSlatt@users.noreply.github.com>
2023-05-31 05:58:43 +0100

    I made a cursor trail and changed the cursor and changed the page font!

---
+++ scripts/index.js
@@ -0,0 +1,102 @@
+/*check for login info here*/
+
+function myFunction() {
+  /* checks values to matching username and password*/
+  if (
+    document.getElementById("user").value === "yes" &&
+    document.getElementById("pass").value == "yes"
+  ) {
+    /*goes to dashboard page*/
+    window.location.href = "../view/dashboard.html";
+  }
+}
+
+/* Object Database */
+let profile1 = {
+  firstName: "Joe",
+  lastName: "Mama",
+  balance: 1500,
+  savings: 5000,
+  username: "joemama",
+  password: "password2021",
+};
+let profile2 = {
+  firstName: "Mike",
+  lastName: "Hawk",
+  balance: 2300,
+  savings: 120,
+  username: "mikehawk",
+  password: "password2022",
+};
+let profile3 = {
+  firstName: "Ben",
+  lastName: "Dover",
+  balance: 12,
+  savings: 50000,
+  username: "bendover",
+  password: "password2023",
+};
+
+/* Hmmm */
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e",
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
