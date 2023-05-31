57de049dcc85c933bacce8dbd6ae3a9241ca6f9c
prodSlatt <124645710+prodSlatt@users.noreply.github.com>
2023-05-31 05:01:32 +0100

    Styled the page using Css

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-/* PASSWORD CHANGER*/
-if (newPass != verifyPass) {
-}
-/* return to login page*/
-function returnLogin(){
-  window.location.href = "index.html";
-}
-
 /*check for login info here*/
 
 function myFunction() {
-  if (document.getElementById("user").value === "joemama" && document.getElementById("pass").value === "password2021"){
-  window.location.href = ("dashboard.html");
-}
-  if (document.getElementById("user").value === "mikehawk" && document.getElementById("pass").value === "password2022"){
-  window.location.href = "dashboard.html";
-}
-  if (document.getElementById("user").value === "bendover" && document.getElementById("pass").value === "password2023"){
-  window.location.href = "dashboard.html";
-}
-}
+  /* checks values to matching username and password*/
+  if (
+    document.getElementById("user").value === "joemama" &&
+    document.getElementById("pass").value === "password2021"
+  ) {
+    /*goes to dashboard page*/
+    window.location.replace =
+      "C:\\Users\\gamin\\OneDrive\\Desktop\\NEW SHIT\\BANKING APP\\dashboard.html";
+    console.log(document.getElementById("user").value);
+    console.log(document.getElementById("pass").value);
+  }
 
+  if (
+    document.getElementById("user").value === "mikehawk" &&
+    document.getElementById("pass").value === "password2022"
+  ) {
+    window.location.href = "dashboard.html";
+  }
 
-
+  if (
+    document.getElementById("user").value === "bendover" &&
+    document.getElementById("pass").value === "password2023"
+  ) {
+    window.location.href = "dashboard.html";
+  }
+}
 
 /* Object Database */
 let profile1 = {
@@ -29,24 +34,24 @@ let profile1 = {
   lastName: "Mama",
   balance: 1500,
   savings: 5000,
-  username: "joemama";
-  password: "password2021";
+  username: "joemama",
+  password: "password2021",
 };
 let profile2 = {
   firstName: "Mike",
   lastName: "Hawk",
   balance: 2300,
   savings: 120,
-  username: "mikehawk";
-  password: "password2022";
+  username: "mikehawk",
+  password: "password2022",
 };
 let profile3 = {
   firstName: "Ben",
   lastName: "Dover",
   balance: 12,
   savings: 50000,
-  username: "bendover";
-  password: "password2023";
+  username: "bendover",
+  password: "password2023",
 };
 
 /* Hmmm */
