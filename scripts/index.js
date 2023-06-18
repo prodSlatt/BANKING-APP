1ba6c5d0f5cfca7b851fe0173878515da7e1035a
prodSlatt <124645710+prodSlatt@users.noreply.github.com>
2023-06-18 02:22:31 +0100

    Stored each username and password in its own object, they must match to login.

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,46 @@
 /*check for login info here*/
 
+/* Object Database */
+let profiles = [
+  {
+    firstName: "Joe",
+    lastName: "Mama",
+    balance: 1500,
+    savings: 5000,
+    username: "joemama",
+    password: "password2021",
+  },
+
+  {
+    firstName: "Mike",
+    lastName: "Hawk",
+    balance: 2300,
+    savings: 120,
+    username: "mikehawk",
+    password: "password2022",
+  },
+
+  {
+    firstName: "Ben",
+    lastName: "Dover",
+    balance: 12,
+    savings: 50000,
+    username: "bendover",
+    password: "password2023",
+  },
+];
+
 function myFunction() {
-  /* checks values to matching username and password*/
-  if (
-    document.getElementById("user").value === "yes" &&
-    document.getElementById("pass").value == "yes"
-  ) {
-    /*goes to dashboard page*/
-    window.location.href = "../view/dashboard.html";
+  for (var i = 0; i < profiles.length; i++) {
+    var account = profiles[i];
+    if (
+      document.getElementById("user").value === account.username &&
+      document.getElementById("pass").value === account.password
+    ) {
+      window.location.href = "../view/dashboard.html";
+    }
   }
 }
-
-/* Object Database */
-let profile1 = {
-  firstName: "Joe",
-  lastName: "Mama",
-  balance: 1500,
-  savings: 5000,
-  username: "joemama",
-  password: "password2021",
-};
-let profile2 = {
-  firstName: "Mike",
-  lastName: "Hawk",
-  balance: 2300,
-  savings: 120,
-  username: "mikehawk",
-  password: "password2022",
-};
-let profile3 = {
-  firstName: "Ben",
-  lastName: "Dover",
-  balance: 12,
-  savings: 50000,
-  username: "bendover",
-  password: "password2023",
-};
 
 /* Hmmm */
 document.addEventListener("mousemove", function (e) {
