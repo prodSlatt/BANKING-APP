f777bfc1f71568c6df64973ccea915c7ed499860
prodSlatt <124645710+prodSlatt@users.noreply.github.com>
2023-06-26 00:25:49 +0100

    Added a a pie chart

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,4 +1,4 @@
-const loggedInProfile = JSON.parse(localStorage.getItem('loggedInProfile'));
+const loggedInProfile = JSON.parse(localStorage.getItem("loggedInProfile"));
 
 // Check if the loggedInProfile exists
 if (loggedInProfile) {
@@ -10,7 +10,72 @@ if (loggedInProfile) {
   console.log(loggedInProfile.username);
   console.log(loggedInProfile.password);
 } else {
-  console.log('Logged in profile not found.');
+  console.log("Logged in profile not found.");
 }
 
+const chartData = {
+  labels: ["Python", "Java", "JavaScript", "C#", "Others"],
+  data: [30, 17, 10, 7, 36],
+};
 
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programming-stats .details ul");
+
+new Chart(myChart, {
+  type: "doughnut",
+  data: {
+    labels: chartData.labels,
+    datasets: [
+      {
+        label: "Language Popularity",
+        data: chartData.data,
+      },
+    ],
+  },
+  options: {
+    borderWidth: 10,
+    borderRadius: 2,
+    hoverBorderWidth: 0,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+const populateUl = () => {
+  chartData.labels.forEach((l, i) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+    ul.appendChild(li);
+  });
+};
+
+populateUl();
+
+var chrt = document.getElementById("chartId").getContext("2d");
+var chartId = new Chart(chrt, {
+  type: "doughnut",
+  data: {
+    labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
+    datasets: [
+      {
+        label: "online tutorial subjects",
+        data: [20, 40, 13, 35, 20, 38],
+        backgroundColor: [
+          "yellow",
+          "aqua",
+          "pink",
+          "lightgreen",
+          "gold",
+          "lightblue",
+        ],
+        hoverOffset: 5,
+      },
+    ],
+  },
+  options: {
+    responsive: false,
+  },
+});
--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,89 @@
-      const profiles = [
-        {
-          firstName: "Joe",
-          lastName: "Mama",
-          balance: 1500,
-          savings: 5000,
-          username: "joemama",
-          password: "password2021",
-        },
-        {
-          firstName: "Mike",
-          lastName: "Hawk",
-          balance: 2300,
-          savings: 120,
-          username: "mikehawk",
-          password: "password2022",
-        },
-        {
-          firstName: "Ben",
-          lastName: "Dover",
-          balance: 12,
-          savings: 50000,
-          username: "bendover",
-          password: "password2023",
-        },
-      ];
+const profiles = [
+  {
+    firstName: "Joe",
+    lastName: "Mama",
+    balance: 1500,
+    savings: 5000,
+    username: "joemama",
+    password: "password2021",
+  },
+  {
+    firstName: "Mike",
+    lastName: "Hawk",
+    balance: 2300,
+    savings: 120,
+    username: "mikehawk",
+    password: "password2022",
+  },
+  {
+    firstName: "Ben",
+    lastName: "Dover",
+    balance: 12,
+    savings: 50000,
+    username: "bendover",
+    password: "password2023",
+  },
+];
 
-    function login() {
-      const username = document.getElementById("user").value;
-      const password = document.getElementById("pass").value;
+function login() {
+  const username = document.getElementById("user").value;
+  const password = document.getElementById("pass").value;
 
-      const profile = profiles.find(
-        (profile) => profile.username === username && profile.password === password
-      );
+  const profile = profiles.find(
+    (profile) => profile.username === username && profile.password === password
+  );
 
-      if (profile) {
-        // Save the profile in local storage
-        localStorage.setItem('loggedInProfile', JSON.stringify(profile));
+  if (profile) {
+    // Save the profile in local storage
+    localStorage.setItem("loggedInProfile", JSON.stringify(profile));
 
-        // Redirect to the dashboard page
-        window.location.href = "view/dashboard.html";
-      } 
+    // Redirect to the dashboard page
+    window.location.href = "view/dashboard.html";
+  }
 
-      // Display an error message
-      document.getElementById("error").textContent = "Incorrect username or password.";
-      
-    }
+  // Display an error message
+  document.getElementById("error").textContent =
+    "Incorrect username or password.";
+}
+
+/* SPLIT HERE*/
+const chartData = {
+  labels: ["Python", "Java", "JavaScript", "C#", "Others"],
+  data: [30, 17, 10, 7, 36],
+};
+
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programming-stats .details ul");
+
+new Chart(myChart, {
+  type: "doughnut",
+  data: {
+    labels: chartData.labels,
+    datasets: [
+      {
+        label: "Language Popularity",
+        data: chartData.data,
+      },
+    ],
+  },
+  options: {
+    borderWidth: 10,
+    borderRadius: 2,
+    hoverBorderWidth: 0,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+const populateUl = () => {
+  chartData.labels.forEach((l, i) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+    ul.appendChild(li);
+  });
+};
+
+populateUl();
